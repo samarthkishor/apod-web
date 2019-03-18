a99e7f18455592308116ad4221c4fee30e69c993
Samarth Kishor <samarthkishor1@gmail.com>
2019-03-17 22:56:59 -0400

    Updates

---
+++ precache-manifest.0c05e5e273933495d4f2e368dac93ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ef09cd673df3c14739e",
+    "url": "/apod-web/static/css/main.47c9aa7e.chunk.css"
+  },
+  {
+    "revision": "6ef09cd673df3c14739e",
+    "url": "/apod-web/static/js/main.bde919aa.chunk.js"
+  },
+  {
+    "revision": "a27a4ed45eae3ece1ab0",
+    "url": "/apod-web/static/js/runtime~main.023eacab.js"
+  },
+  {
+    "revision": "fae15ebae590f9ddf7ed",
+    "url": "/apod-web/static/css/2.c90993e7.chunk.css"
+  },
+  {
+    "revision": "fae15ebae590f9ddf7ed",
+    "url": "/apod-web/static/js/2.2c2fa41f.chunk.js"
+  },
+  {
+    "revision": "9ac173e025e0161d3a3d0c4ad5da7c49",
+    "url": "/apod-web/index.html"
+  }
+];
